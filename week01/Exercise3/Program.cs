3eae1993009a58f61df3dc37384303fdd5103e9d
salomeybtoah <salomeybempomaatoah@gmail.com>
2026-09-06 23:37:37 +0000

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        // Outer loop for playing again
+        while (playAgain == "yes")
+        {
+            // Generate a random number from 1 to 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("\nI'm thinking of a number between 1 and 100...");
+
+            // Inner loop for guessing
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessCount} guesses.");
+                }
+            }
+
+            // Ask if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+            // Handle invalid input
+            while (playAgain != "yes" && playAgain != "no")
+            {
+                Console.Write("Please enter 'yes' or 'no': ");
+                playAgain = Console.ReadLine().ToLower();
+            }
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
